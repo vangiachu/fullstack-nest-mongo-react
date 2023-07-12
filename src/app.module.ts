780ffad12422df73cd98229085f628150b58dd13
Chu Văn Giá <vangiachu@gmail.com>
2023-07-12 11:01:04 +0700

    first commit

---
+++ src/app.module.ts
@@ -0,0 +1,15 @@
+import { Module } from '@nestjs/common';
+import { AppController } from './app.controller';
+import { AppService } from './app.service';
+import { MongooseModule } from '@nestjs/mongoose';
+import { TodosModule } from './todos/todos.module';
+
+@Module({
+  imports: [
+    MongooseModule.forRoot('mongodb://127.0.0.1:27017/gia-todo'),
+    TodosModule,
+  ],
+  controllers: [AppController],
+  providers: [AppService],
+})
+export class AppModule {}
---
+++ src/todos/todos.service.ts
@@ -0,0 +1,48 @@
+import { Injectable, NotFoundException } from '@nestjs/common';
+import { CreateTodoDto } from './dto/create-todo.dto';
+import { UpdateTodoDto } from './dto/update-todo.dto';
+import { InjectModel } from '@nestjs/mongoose';
+import { Todo, TodoDocument } from './entities/todo.entity';
+import { Model } from 'mongoose';
+
+@Injectable()
+export class TodosService {
+  constructor(
+    @InjectModel(Todo.name) private readonly todoModel: Model<TodoDocument>,
+  ) {}
+
+  async create(createTodoDto: CreateTodoDto) {
+    const todo = new this.todoModel(createTodoDto);
+    return await todo.save();
+  }
+
+  async findAll() {
+    return await this.todoModel.find().exec();
+  }
+
+  async findOne(id: string) {
+    return await this.todoModel.findById(id).exec();
+  }
+
+  async update(id: string, updateTodoDto: UpdateTodoDto) {
+    const todo = await this.todoModel
+      .findByIdAndUpdate(
+        id,
+        { title: updateTodoDto.title, completed: updateTodoDto.completed },
+        { new: true },
+      )
+      .exec();
+    if (!todo) {
+      throw new NotFoundException();
+    }
+    return todo;
+  }
+
+  async remove(id: string) {
+    const todo = await this.todoModel.findByIdAndRemove(id).exec();
+    if (!todo) {
+      throw new NotFoundException();
+    }
+    return todo;
+  }
+}
